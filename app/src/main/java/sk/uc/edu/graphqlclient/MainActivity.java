5341edab6faa2e47f48a5287c11f26ed4683e0ca
Martin Malobický <malobicky.m@gmail.com>
2018-03-11 22:05:04 +0100

    3 _ main activity load action initialized, main model set up, swap action done

--- app/src/main/java/sk/uc/edu/graphqlclient/MainActivity.java
+++ app/src/main/java/sk/uc/edu/graphqlclient/MainActivity.java
@@ -1,26 +1,72 @@
 package sk.uc.edu.graphqlclient;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import sk.uc.edu.graphqlclient.adapter.Adapter;
+import sk.uc.edu.graphqlclient.addingForm.AddItemActivity;
+import sk.uc.edu.graphqlclient.viewModel.MainActivityViewModel;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int ADD_ITEM_REQUEST_CODE = 1;
+    private Adapter adapter;
+
+    private RecyclerView recyclerView;
     private Button button;
     private EditText sendEditText;
+
+    private MainActivityViewModel mainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initLayout();
+
+        mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        adapter = new Adapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+        mainActivityViewModel.getTodosData().observe(this, data -> {
+            if (data != null) {
+                adapter.swapItems(data.getAll);
+            }
+        });
+
+        sendRequest();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mainActivityViewModel.loadData();
+    }
+
+    private void sendRequest() {
+        mainActivityViewModel.loadData();
+    }
+
+    private void addItem() {
+        //mainActivityViewModel.addItem();
+        Intent intent = new Intent(this, AddItemActivity.class);
+        startActivityForResult(intent, ADD_ITEM_REQUEST_CODE);
     }
 
     private void initLayout() {
         button = findViewById(R.id.activity_main__send_button);
+        button.setOnClickListener(v -> sendRequest());
+        recyclerView = findViewById(R.id.activity_main__recycler_view);
 
         sendEditText = findViewById(R.id.activity_main__add_item__button);
         sendEditText.setFocusable(false);
+        sendEditText.setOnClickListener(v -> addItem());
     }
 }
--- app/src/main/java/sk/uc/edu/graphqlclient/viewModel/MainActivityViewModel.java
+++ app/src/main/java/sk/uc/edu/graphqlclient/viewModel/MainActivityViewModel.java
@@ -1,6 +1,18 @@
 package sk.uc.edu.graphqlclient.viewModel;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import javax.annotation.Nonnull;
+
+import sk.uc.edu.graphqlclient.GetTodosQuery;
+import sk.uc.edu.graphqlclient.apolloClient.MyApolloClient;
 
 /**
  * Created by malobicky
@@ -10,8 +22,36 @@ import android.arch.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
 
+    private ApolloClient myApolloClient;
+
+    private MutableLiveData<GetTodosQuery.Data> todosData;
+
     public MainActivityViewModel() {
 
+        myApolloClient = MyApolloClient.getMyApolloClient();
+        todosData = new MutableLiveData<>();
     }
 
+    public LiveData<GetTodosQuery.Data> getTodosData() {
+        return todosData;
+    }
+
+    public void loadData() {
+        myApolloClient.query(GetTodosQuery.builder().build()).enqueue(new ApolloCall.Callback<GetTodosQuery.Data>() {
+            @Override
+            public void onResponse(@Nonnull Response<GetTodosQuery.Data> response) {
+                if (response.data() != null) {
+                    GetTodosQuery.Data data = response.data();
+                    if (data != null) {
+                        todosData.postValue(data);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@Nonnull ApolloException e) {
+                //Toast.makeText(MainActivity.this, "Failure called " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
